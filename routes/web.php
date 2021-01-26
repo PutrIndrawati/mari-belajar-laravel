2d7fcc7895590d98ceb01b77dd363cb7d2bb2337
Rizky Pamungkas <46834814+VzMaster31@users.noreply.github.com>
2021-01-26 11:25:31 +0700

    Revisi error dan file laravel yang rusak

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/',function(){
 	return view ('home');
 });
@@ -15,3 +26,25 @@ Route::get('/data',function(){
 
 
 Route::post('/data', 'SiswiController@store');
+
+
+// Closure
+Route::get('/profil', function(){
+    return"ini adalah profil siswi";
+})->name('profil');
+
+// Route name
+Route::get("/tesname", function(){
+    return route('profil');
+});
+
+// Parameter
+Route::get('/profil/{id}', function($id){
+    return $id;
+});
+
+// Controller
+Route::get("/tescontroller", "TesController@show");
+
+// Resource
+Route::resource('siswi', "SiswiController");
